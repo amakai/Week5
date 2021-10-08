7944fb69019ba354ff38e7561eaf3eb5f4944978
Amanda <amanda.kaijanen@gmail.com>
2021-10-08 17:25:28 +0300

    add new projects

---
+++ ForLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I will not skateboard in the halls.
+
+           
+
+            for (int i = 0; i < 10; i++) //inti = 0; start; i<10; - end; i++ = i = i + 1- stop
+            {
+                Console.WriteLine($" {i+1}. I will not skateboard in the halls.".ToUpper());
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
